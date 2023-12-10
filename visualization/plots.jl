a9f818273ac0cc7e699c63d8d624e4795bc50841
Sergey Bars <ssbars137@gmail.com>
2023-12-10 08:57:07 +0400

    plots

---
+++ visualization/plots.jl
@@ -0,0 +1,61 @@
+using Plots
+using Tables
+using CSV
+
+lcg = CSV.File("visualization/data/lcg.csv") |>  Tables.matrix
+
+norm2d = CSV.File("visualization/data/norm2d_deg.csv") |>  Tables.matrix
+
+beta = CSV.File("visualization/data/beta.csv") |>  Tables.matrix
+
+beta_sampled = CSV.File("visualization/data/beta_sampling.csv") |>  Tables.matrix
+
+begin
+    
+    p = histogram(lcg[:,2],
+                    bins=30,
+                    xlabel="Value",
+                    ylabel="Frequency",
+                    title="Histogram",
+                    label = "Linear congruent generator"
+        )
+end
+
+
+begin
+    scatter(norm2d[:,1],norm2d[:,2])
+end
+
+
+begin
+    histogram2d(norm2d[:,1],norm2d[:,2], bins=50)
+end
+
+
+begin
+    scatter(beta[:,1], beta[:,2])
+end
+
+begin
+
+    xx = beta[:, 1]
+    yy = beta[:, 2]
+    p_star = maximum(yy)
+
+    (acc_samples, acc_u) = (beta_sampled[:, 1],  beta_sampled[:, 2]);
+
+    plot1 = plot(xx, yy, color=:black, linewidth=2, label="");
+    plot!(plot1, [0, 1], [p_star, p_star], color=:red, linewidth=2, linestyle=:dash, label="");
+    scatter!(plot1, acc_samples, acc_u, color=:red, markersize=2, label="");
+    xlims!(plot1, 0, 1);
+    ylims!(plot1, 0, 2.8);
+
+    # Second subplot
+    plot2 = plot()
+    plot!(plot2, xx, yy, color=:red, linewidth=2, label="");
+    histogram!(plot2, acc_samples, bins=100, normalized=true, color=:black, alpha=0.7, label="", opacity = 0.4);
+    xlims!(plot2, 0, 1);
+
+    plot(plot1, plot2, layout=(1, 2))
+
+end
